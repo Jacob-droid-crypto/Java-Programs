02442e575e6ea6a663688f8def7bfc23684347b0
Jacob Thomas <jacobthomassjcet@gmail.com>
2025-08-06 15:26:52 +0530

    Add files via upload

--- BookLibrary.java
+++ BookLibrary.java
@@ -1,9 +1,45 @@
+class Boook{
+    String title;
+    String author;
+    final int boookID;
+
+    static int bookCounter = 1000;
+    static final String LIBRARY_NAME = "Central Library";
+
+    Boook() {
+        title = "Not mentioned";
+        author = "Not mentioned";
+        boookID = bookCounter++;
+    }
+
+    Boook(String title, String author) {
+        this.title = title;
+        this.author = author;
+        boookID = bookCounter++;
+    }
+
+    public void displayInfo() {
+        System.out.println("Book Title: " + title);
+        System.out.println("Book Author: " + author);
+        System.out.println("Book ID: " + boookID);
+    }
+
+    public void displayInfo(boolean showLibrary) {
+        displayInfo();
+        if (showLibrary) {
+            System.out.println("Library Name: " + LIBRARY_NAME);
+        }
+    }
+
+    public static void displayTotalBooks() {
+        System.out.println("Total Books: " + (bookCounter - 1000));
+    }
+}
 
 public class BookLibrary {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Boook book = new Boook();
+        book.displayInfo(true);     
+        book.displayTotalBooks();  
+    }
 }
